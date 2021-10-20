94124a276e3cd18ccbc5cf22e69f8b5b1c9ffb0c
Frederic Bilodeau <freder202@live.ca>
2021-10-20 05:00:07 -0400

    commit

--- Client.java
+++ Client.java
@@ -1,13 +1,20 @@
 import java.io.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+
+      String currentFolder =  System.getProperty("user.dir");
 
         DataInputStream in = new DataInputStream(System.in);
         BufferedReader d = new BufferedReader(new InputStreamReader(in));
         System.out.println("Enter file name: ");
         String file = d.readLine();
-
+        file = currentFolder + "\\" + file;
+        System.out.println(file);
         Application coucheApplication = Application.getInstance();
         Couche coucheTransport = Transport.getInstance();
         Couche coucheliaison = LiaisonDeDonnees.getInstance();
